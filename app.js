2168baa98bbf2a086e48b864e965d9293f1bcbad
shankar <gowrishankarirwog@gmail.com>
2022-04-26 02:08:38 +0530

    multer get image

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
+require("dotenv").config();
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const multer = require("multer")
 const morgan = require("morgan");
 
 const app = express();
+const upload = multer({});
 const PORT = process.env.PORT || 3080;
 
 app.use(morgan("tiny"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
 
-app.get("/api", (req,res) => {
+function createToken(_id, maxAge) {
+	const token = jwt.sign({ _id }, process.env.JWT_SECRET_KEY, { expiresIn: maxAge });
+	return token;
+}
+
+app.post("/api/signup", upload.single('pic'), async (req, res) => {
+	try {
+		const encoded = req.file.buffer.toJSON()
+		console.log(encoded);
+		res.json({msg: 'done'})
+	} catch (err) {
+		console.error(err);
+	}
+});
+
+app.get("/api", (req, res) => {
 	const data = {
 		username: "random name",
 		age: -1,
