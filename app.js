609c4558a825cca00a1efbeb9c301b101e9ce568
shankar <gowrishankarirwog@gmail.com>
2022-04-26 19:02:49 +0530

    basic signup setup

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
-const multer = require("multer")
+const multer = require("multer");
 const morgan = require("morgan");
+
+const { compare, compareImgs, getEmbeddings } = require("./face_recognition/recognize.js");
+const { User } = require("./models/userModel.js");
 
 const app = express();
 const upload = multer({});
@@ -14,18 +18,33 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
+mongoose
+	.connect(process.env.MONGO_URI)
+	.then(() => console.log("Successful DB connection"))
+	.catch((err) => console.error("DB connection fail"));
+
 function createToken(_id, maxAge) {
 	const token = jwt.sign({ _id }, process.env.JWT_SECRET_KEY, { expiresIn: maxAge });
 	return token;
 }
 
-app.post("/api/signup", upload.single('pic'), async (req, res) => {
+app.post("/api/signup", upload.single("pic"), async (req, res) => {
 	try {
-		const encoded = req.file.buffer.toJSON()
-		console.log(encoded);
-		res.json({msg: 'done'})
+		const userExists = await User.exists({ email: req.body.email });
+		if (userExists) throw new Error("User Already Exists");
+
+		const userEbd = await getEmbeddings(req.file.buffer);
+		const userEbdArr = await userEbd.array();
+
+		const user = { email: req.body.email, name: req.body.name, facialEmbeddings: userEbdArr };
+		const u = await User.create(user);
+
+		const tok = createToken(u._id, "2h");
+		res.cookie("engage_jwt", tok, { maxAge: 2 * 60 * 60 * 1000, httpOnly: true });
+		res.status(200).json({ msg: "User Created Successfully" });
 	} catch (err) {
 		console.error(err);
+		res.status(400).json(err);
 	}
 });
 
--- face_recognition/recognize.js
+++ face_recognition/recognize.js
@@ -1,31 +1,44 @@
 const tf = require("@tensorflow/tfjs-node");
-const fs = require("fs");
-const path = require("path");
 
-var img1 = fs.readFileSync(path.resolve(__dirname, "./asserts/Amanda Crew0_0.jpg"));
-var img2 = fs.readFileSync(path.resolve(__dirname, "./asserts/Amanda Crew107_7.jpg"));
+const ArcFace_MODEL_PTH = "file://./face_recognition/arcface/model.json";
+const ArcFace_INPUT_SHAPE = [112, 112];
+const ArcFace_THRESHOLD = 0.68;
 
-const processImg = (img) => {
-	img = tf.node.decodeImage(img);
+const processImg = (imgBuf) => {
+	var img = tf.node.decodeImage(imgBuf);
 	img = tf.cast(img, "float32");
-	img = tf.image.resizeBilinear(img, [112, 112]);
+	img = tf.image.resizeBilinear(img, ArcFace_INPUT_SHAPE);
 	img = tf.expandDims(img, 0);
 	img = tf.div(img, 255);
 	return img;
 };
 
-const cosineDistance = (ebd1, ebd2) => {
+const cosineDistance = async (ebd1, ebd2) => {
 	ebd1 = tf.div(ebd1, ebd1.norm());
 	ebd2 = tf.div(ebd2, ebd2.norm());
 	return tf.losses.cosineDistance(ebd1, ebd2);
 };
 
-const compare = async (img1, img2) => {
-	const model = await tf.loadLayersModel("file://./face_recognition/arcface/model.json");
-	const ebd1 = model.predict(processImg(img1));
-	const ebd2 = model.predict(processImg(img2));
-	const dist = cosineDistance(ebd1, ebd2);
+const getEmbeddings = async (imgBuf, path = ArcFace_MODEL_PTH) => {
+	var model = await tf.loadLayersModel(path);
+	var img = processImg(imgBuf);
+	var ebd = model.predict(img);
+	var ebd = tf.reshape(ebd, [-1])
+	return ebd;
+};
+
+const compareImgs = async (img1, img2) => {
+	var ebd1 = await getEmbeddings(img1);
+	var ebd2 = await getEmbeddings(img2);
+	var dist = cosineDistance(ebd1, ebd2);
 	return dist;
 };
 
-compare(img1, img2).then((d) => d.print());
+const compare = async (anchorEbd, imgBuffer, threshold = ArcFace_THRESHOLD) => {
+	var imgEbd = await getEmbeddings(imgBuffer);
+	var cosDist = await cosineDistance(anchorEbd, imgEbd);
+	var similar = await cosDist.array() <= threshold;
+	return similar;
+};
+
+module.exports = { compare, compareImgs, getEmbeddings };
