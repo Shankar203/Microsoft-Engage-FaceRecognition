e52680bab2c31fae20823d7c8d48e80c59e1b3a1
shankar <gowrishankarirwog@gmail.com>
2022-05-24 22:52:58 +0530

    deploy azure

--- routes/protectedRoutes.js
+++ routes/protectedRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { isLoggedin } = require("../middlewares/userAuth.js");
 
-router.get("/", isLoggedin, (req, res, next) => {
+router.post("/", isLoggedin, (req, res, next) => {
 	res.status(200).json({ msg: "You can access the page" });
 });
 
